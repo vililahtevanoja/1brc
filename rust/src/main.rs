f3a574631da33ff04bd773f4e98e0e623a11041c
Vili Lähtevänoja <vili.lahtevanoja@gmail.com>
2024-03-04 19:28:13 +0200

    Further improvements including in-place custom parse & add

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -13,8 +13,10 @@ use rayon::prelude::*;
 use rustc_hash::FxHashMap;
 use std::{
     collections::HashMap,
+    env,
     fs::File,
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, Read},
+    simd::{num::SimdInt, Simd},
 };
 
 #[derive(Debug, Copy, Clone)]
@@ -59,8 +61,122 @@ impl MeasurementStats {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+struct MeasurementStatsInt {
+    min: i32,
+    max: i32,
+    sum: i32,
+    count: usize,
+}
+
+impl MeasurementStatsInt {
+    fn new(val: i32) -> Self {
+        MeasurementStatsInt {
+            min: val,
+            max: val,
+            sum: val,
+            count: 1,
+        }
+    }
+
+    fn add(&self, val: i32) -> Self {
+        MeasurementStatsInt {
+            min: i32::min(self.min, val),
+            max: i32::max(self.max, val),
+            sum: self.sum + val,
+            count: self.count + 1,
+        }
+    }
+
+    fn add_mut(&mut self, val: i32) {
+        self.min = i32::min(self.min, val);
+        self.max = i32::max(self.max, val);
+        self.sum += val;
+        self.count += 1;
+    }
+
+    fn merge_mut(&mut self, right: &Self) {
+        self.min = i32::min(self.min, right.min);
+        self.max = i32::max(self.max, right.max);
+        self.sum = self.sum + right.sum;
+        self.count = self.count + right.count;
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct MeasurementStatsMask {
+    min_max: i32,
+    sum_count: i64,
+}
+
+impl MeasurementStatsMask {
+    fn new(val: f32) -> Self {
+        let v = (val * 10.0) as i32;
+
+        MeasurementStatsMask {
+            min_max: (v << 16) & v as i32,
+            sum_count: ((v as i64) << 32) & 1i64,
+        }
+    }
+
+    fn add(&self, val: f32) -> Self {
+        let v = (val * 10.0) as i32;
+        let min = i32::min(self.min_max >> 16, v);
+        let max = i32::max((self.min_max << 16) >> 16, v);
+        let sum = (self.sum_count >> 32) + v as i64;
+        let count = ((self.sum_count << 32) >> 32) + 1;
+
+        MeasurementStatsMask {
+            min_max: (min << 16) & max,
+            sum_count: (sum << 32) & count,
+        }
+    }
+
+    fn add_mut(&mut self, val: f32) {
+        let v = (val * 10.0) as i32;
+        let min = i32::min(self.min_max >> 16, v);
+        let max = i32::max((self.min_max << 16) >> 16, v);
+        let sum = (self.sum_count >> 32) + v as i64;
+        let count = ((self.sum_count << 32) >> 32) + 1;
+        self.min_max = (min << 16) & max;
+        self.sum_count = (sum << 32) & count;
+    }
+
+    fn merge_mut(&mut self, right: &Self) {
+        let min = i32::min(self.min_max >> 16, right.min_max >> 16);
+        let max = i32::max(self.min_max << 16, right.min_max << 16);
+        self.min_max = (min << 16) & max;
+        let sum = (self.sum_count >> 32) + (right.sum_count >> 32);
+        let count = ((self.sum_count << 32) >> 32) + ((right.sum_count << 32) >> 32);
+        self.sum_count = (sum << 32) & count;
+    }
+}
+
 fn main() {
-    let res = handle_fhash_ffloat_read_until_mut_par("../_data/measurements_1b.txt");
+    let args: Vec<String> = env::args().collect();
+    let file = "../_data/measurements_1b.txt";
+    if args.len() == 1 {
+        println!(
+            "{}",
+            handle_fhash_dint_read_until_mut_cparse_par("../_data/measurements_1b.txt")
+        )
+    }
+    let res = match args[1].as_str() {
+        "00" => handle_naive(file),
+        "01" => handle_fhash(file),
+        "02" => handle_fhash_ffloat(file),
+        "03" => handle_fhash_ffloat_read_until(file),
+        "04" => handle_fhash_lookup_read_until(file),
+        "05" => handle_fhash_ffloat_read_until_mut(file),
+        "06" => handle_fhash_dfloat_read_until_mut(file),
+        "07" => handle_fhash_dint_read_until_mut(file),
+        "08" => handle_fhash_dint_read_until_mut_memchr(file),
+        "09" => handle_fhash_dint_read_until_mut_memchr_cparse(file),
+        "10" => handle_fhash_dint_read_until_mut_cparse_par(file),
+        _ => handle_fhash_dint_read_until_mut_cparse_par(file),
+    };
+    let res = handle_fhash_dint_read_until_mut_cparse_par("../_data/measurements_1b.txt");
+    // let res = handle_fhash_dint_read_until_mut_memchr_cparse("../_data/measurements_1b.txt");
     println!("{}", res)
 }
 
@@ -290,6 +406,244 @@ fn handle_fhash_ffloat_read_until_mut(file: &str) -> String {
     format!("{{{}}}", results.join(", "))
 }
 
+/**
+ * Dum dum parsing function to make use of the knowledge that
+ * the values are all between -99.9 and 99.9.
+ */
+#[inline(always)]
+fn dum_parse(bs: &[u8]) -> f32 {
+    let mut idx = bs.len();
+    let sign_factor = match bs[0] {
+        b'-' => -0.1,
+        _ => 0.1,
+    };
+    let stop_idx = if sign_factor > 0.0 { 0 } else { 1 };
+    let factors = Simd::from_array([0, 100, 10, 1]);
+    let mut components: Simd<i32, 4> = Simd::splat(0i32);
+    let mut simd_idx = 3usize;
+    while idx > stop_idx {
+        let c = bs[idx - 1];
+        if c == b'.' {
+            idx -= 1;
+            continue;
+        }
+        components[simd_idx] = (c - b'0') as i32;
+        idx -= 1;
+        simd_idx -= 1;
+    }
+    ((factors * components).reduce_sum() as f32) * sign_factor
+}
+
+/**
+ * Dum dum parsing function to make use of the knowledge that
+ * the values are all between -99.9 and 99.9.
+ */
+#[inline(always)]
+fn dum_parse_as_i32(bs: &[u8]) -> i32 {
+    let mut idx = bs.len();
+    let sign_factor = match bs[0] {
+        b'-' => -1,
+        _ => 1,
+    };
+    let stop_idx = if sign_factor > 0 { 0 } else { 1 };
+    let factors = Simd::from_array([0, 100, 10, 1]);
+    let mut components: Simd<i32, 4> = Simd::splat(0i32);
+    let mut simd_idx = 3usize;
+    while idx > stop_idx {
+        let c = bs[idx - 1];
+        if c == b'.' {
+            idx -= 1;
+            continue;
+        }
+        components[simd_idx] = (c - b'0') as i32;
+        idx -= 1;
+        simd_idx -= 1;
+    }
+    ((factors * components).reduce_sum()) * sign_factor
+}
+
+fn handle_fhash_dfloat_read_until_mut(file: &str) -> String {
+    let file = File::open(file).unwrap();
+    let mut m: FxHashMap<Vec<u8>, MeasurementStats> = FxHashMap::default();
+    let mut reader = BufReader::new(file);
+    let mut buf = Vec::with_capacity(256);
+    while let Ok(n) = reader.read_until(b'\n', &mut buf) {
+        if n == 0 {
+            break;
+        }
+        let s = &buf[..n - 1];
+        if let Some((city, measurement_str)) = s.split_once(|&c| c == b';') {
+            let measurement = dum_parse(measurement_str);
+            if let Some(existing) = m.get_mut(city) {
+                existing.add_mut(measurement);
+            } else {
+                m.insert(city.to_owned(), MeasurementStats::new(measurement));
+            }
+        }
+        buf.clear();
+    }
+
+    let mut results = m
+        .iter()
+        .map(|(city, measurement)| {
+            format!(
+                "{}={:.1}/{:.1}/{:.1}",
+                String::from_utf8(city.to_owned()).unwrap(),
+                measurement.min,
+                measurement.sum / measurement.count as f32,
+                measurement.max
+            )
+        })
+        .collect::<Vec<_>>();
+    results.sort();
+    format!("{{{}}}", results.join(", "))
+}
+
+fn handle_fhash_dint_read_until_mut(file: &str) -> String {
+    let file = File::open(file).unwrap();
+    let mut m: FxHashMap<Vec<u8>, MeasurementStatsInt> = FxHashMap::default();
+    let mut reader = BufReader::new(file);
+    let mut buf = Vec::with_capacity(64);
+    while let Ok(n) = reader.read_until(b'\n', &mut buf) {
+        if n == 0 {
+            break;
+        }
+        let s = &buf[..n - 1];
+        if let Some((city, measurement_str)) = s.split_once(|&c| c == b';') {
+            let measurement = dum_parse_as_i32(measurement_str);
+            if let Some(existing) = m.get_mut(city) {
+                existing.add_mut(measurement);
+            } else {
+                m.insert(city.to_owned(), MeasurementStatsInt::new(measurement));
+            }
+        }
+        buf.clear();
+    }
+
+    let mut results = m
+        .iter()
+        .map(|(city, measurement)| {
+            format!(
+                "{}={:.1}/{:.1}/{:.1}",
+                String::from_utf8(city.to_owned()).unwrap(),
+                measurement.min as f32 * 0.1,
+                (measurement.sum as f32 * 0.1) / measurement.count as f32,
+                measurement.max as f32 * 0.1
+            )
+        })
+        .collect::<Vec<_>>();
+    results.sort();
+    format!("{{{}}}", results.join(", "))
+}
+
+fn handle_fhash_dint_read_until_mut_memchr(file: &str) -> String {
+    let file = File::open(file).unwrap();
+    let mut m: FxHashMap<Vec<u8>, MeasurementStatsInt> = FxHashMap::default();
+    let mut reader = BufReader::new(file);
+    let mut buf = Vec::with_capacity(64);
+    while let Ok(n) = reader.read_until(b'\n', &mut buf) {
+        if n == 0 {
+            break;
+        }
+        let s = &buf[..n - 1];
+        let pos = memchr::memchr(b';', s).unwrap();
+        let city = &s[..pos];
+        let measurement_str = &s[pos + 1..];
+        let measurement = dum_parse_as_i32(measurement_str);
+        if let Some(existing) = m.get_mut(city) {
+            existing.add_mut(measurement);
+        } else {
+            m.insert(city.to_owned(), MeasurementStatsInt::new(measurement));
+        }
+        buf.clear();
+    }
+
+    let mut results = m
+        .iter()
+        .map(|(city, measurement)| {
+            format!(
+                "{}={:.1}/{:.1}/{:.1}",
+                String::from_utf8(city.to_owned()).unwrap(),
+                measurement.min as f32 * 0.1,
+                (measurement.sum as f32 * 0.1) / measurement.count as f32,
+                measurement.max as f32 * 0.1
+            )
+        })
+        .collect::<Vec<_>>();
+    results.sort();
+    format!("{{{}}}", results.join(", "))
+}
+
+fn parse<R: Read>(r: &mut R) -> FxHashMap<Vec<u8>, MeasurementStatsInt> {
+    let mut read = 0;
+    let mut buf = [0u8; 4096];
+    let mut m: FxHashMap<Vec<u8>, MeasurementStatsInt> = FxHashMap::default();
+    let mut spillover: Vec<u8> = Vec::with_capacity(128);
+    loop {
+        let mut curr = 0;
+        read = r.read(&mut buf).unwrap();
+        if read == 0 {
+            break;
+        }
+        if !spillover.is_empty() {
+            match memchr::memchr(b'\n', &buf[curr..read]) {
+                Some(p) => {
+                    spillover.extend_from_slice(&buf[curr..p]);
+                    let s = &spillover.as_slice();
+                    let pos = memchr::memchr(b';', s).unwrap();
+                    let city = &s[..pos];
+                    let measurement_str = &s[pos + 1..];
+                    let measurement = dum_parse_as_i32(measurement_str);
+                    if let Some(existing) = m.get_mut(city) {
+                        existing.add_mut(measurement);
+                    } else {
+                        m.insert(city.to_owned(), MeasurementStatsInt::new(measurement));
+                    }
+                    spillover.clear();
+                    curr += p + 1;
+                }
+                None => break,
+            }
+        }
+
+        while let Some(p) = memchr::memchr(b'\n', &buf[curr..read]) {
+            let s = &buf[curr..curr + p];
+            let pos = memchr::memchr(b';', s).unwrap();
+            let city = &s[..pos];
+            let measurement_str = &s[pos + 1..];
+            let measurement = dum_parse_as_i32(measurement_str);
+            if let Some(existing) = m.get_mut(city) {
+                existing.add_mut(measurement);
+            } else {
+                m.insert(city.to_owned(), MeasurementStatsInt::new(measurement));
+            }
+            curr += p + 1;
+        }
+        spillover.extend_from_slice(&buf[curr..]);
+    }
+    m
+}
+
+fn handle_fhash_dint_read_until_mut_memchr_cparse(file: &str) -> String {
+    let file = File::open(file).unwrap();
+    let mut reader = BufReader::new(file);
+    let m = parse(&mut reader);
+    let mut results = m
+        .iter()
+        .map(|(city, measurement)| {
+            format!(
+                "{}={:.1}/{:.1}/{:.1}",
+                String::from_utf8(city.to_owned()).unwrap(),
+                measurement.min as f32 * 0.1,
+                (measurement.sum as f32 * 0.1) / measurement.count as f32,
+                measurement.max as f32 * 0.1
+            )
+        })
+        .collect::<Vec<_>>();
+    results.sort();
+    format!("{{{}}}", results.join(", "))
+}
+
 fn len_to_chunks(len: usize, chunk_count: usize) -> Vec<(usize, usize)> {
     let chunk_size = len / chunk_count;
     let mut v = Vec::new();
@@ -305,8 +659,7 @@ fn len_to_chunks(len: usize, chunk_count: usize) -> Vec<(usize, usize)> {
     v
 }
 
-fn parse_chunk(mmap: &Mmap, range: (usize, usize)) -> FxHashMap<Vec<u8>, MeasurementStats> {
-    let mut m: FxHashMap<Vec<u8>, MeasurementStats> = FxHashMap::default();
+fn parse_chunk(mmap: &Mmap, range: (usize, usize)) -> FxHashMap<Vec<u8>, MeasurementStatsInt> {
     let (prelim_start, prelim_end) = range;
     let mut start = prelim_start;
     while *mmap.get(start).unwrap() != b'\n' {
@@ -316,23 +669,13 @@ fn parse_chunk(mmap: &Mmap, range: (usize, usize)) -> FxHashMap<Vec<u8>, Measure
     while *mmap.get(end).unwrap() != b'\n' {
         end += 1;
     }
-    let chunk = &mmap[start..=end];
-    chunk.split(|c| *c == b'\n').for_each(|line| {
-        if let Some((city, measurement_str)) = line.split_once(|&c| c == b';') {
-            let measurement = fast_float::parse(measurement_str).unwrap();
-            if let Some(existing) = m.get_mut(city) {
-                existing.add_mut(measurement);
-            } else {
-                m.insert(city.to_owned(), MeasurementStats::new(measurement));
-            }
-        }
-    });
-    m
+    let mut chunk = &mmap[start + 1..end];
+    return parse(&mut chunk);
 }
 
 fn merge_map(
-    left: &mut FxHashMap<Vec<u8>, MeasurementStats>,
-    right: FxHashMap<Vec<u8>, MeasurementStats>,
+    left: &mut FxHashMap<Vec<u8>, MeasurementStatsInt>,
+    right: FxHashMap<Vec<u8>, MeasurementStatsInt>,
 ) {
     right.iter().for_each(|(k, v)| match left.get_mut(k) {
         Some(measurements) => measurements.merge_mut(v),
@@ -342,7 +685,7 @@ fn merge_map(
     });
 }
 
-fn handle_fhash_ffloat_read_until_mut_par(file: &str) -> String {
+fn handle_fhash_dint_read_until_mut_cparse_par(file: &str) -> String {
     let file = File::open(file).unwrap();
     let mmap = unsafe { MmapOptions::new().map(&file).unwrap() };
     let chunk_idxs = len_to_chunks(mmap.len(), 8);
@@ -363,9 +706,9 @@ fn handle_fhash_ffloat_read_until_mut_par(file: &str) -> String {
             format!(
                 "{}={:.1}/{:.1}/{:.1}",
                 String::from_utf8(city.to_owned()).unwrap(),
-                measurement.min,
-                measurement.sum / measurement.count as f32,
-                measurement.max
+                measurement.min as f32 * 0.1,
+                (measurement.sum as f32 * 0.1) / measurement.count as f32,
+                measurement.max as f32 * 0.1
             )
         })
         .collect::<Vec<_>>();
@@ -422,9 +765,54 @@ mod tests {
     }
 
     #[test]
-    fn test_fhash_lookup_read_until_mut_par() {
+    fn test_dum_parse() {
+        let tests = [
+            ("12.3", 12.3f32),
+            ("1.2", 1.2f32),
+            ("0.1", 0.1f32),
+            ("0.0", 0.0f32),
+            ("-0.1", -0.1f32),
+            ("-1.2", -1.2f32),
+            ("-12.3", -12.3f32),
+        ];
+
+        for (input, expected) in tests {
+            assert_eq!(expected, dum_parse(input.as_bytes()))
+        }
+    }
+
+    #[test]
+    fn test_fhash_dfloat_read_until_mut() {
         let expected = include_str!("../../_data/measurements_1m_result.txt").to_string();
-        let actual = handle_fhash_ffloat_read_until_mut_par("../_data/measurements_1m.txt");
+        let actual = handle_fhash_dfloat_read_until_mut("../_data/measurements_1m.txt");
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_fhash_dint_read_until_mut() {
+        let expected = include_str!("../../_data/measurements_1m_result.txt").to_string();
+        let actual = handle_fhash_dint_read_until_mut("../_data/measurements_1m.txt");
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_fhash_dint_read_until_mut_memchr() {
+        let expected = include_str!("../../_data/measurements_1m_result.txt").to_string();
+        let actual = handle_fhash_dint_read_until_mut_memchr("../_data/measurements_1m.txt");
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_fhash_dfloat_lookup_read_until_mut_par() {
+        let expected = include_str!("../../_data/measurements_1m_result.txt").to_string();
+        let actual = handle_fhash_dint_read_until_mut_cparse_par("../_data/measurements_1m.txt");
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_fhash_dint_read_until_mut_memchr_cparse() {
+        let expected = include_str!("../../_data/measurements_1m_result.txt").to_string();
+        let actual = handle_fhash_dint_read_until_mut_memchr_cparse("../_data/measurements_1m.txt");
         assert_eq!(expected, actual)
     }
 
@@ -466,7 +854,27 @@ mod tests {
     }
 
     #[bench]
-    fn bench_06_fhash_ffloat_read_until_mut_par(b: &mut Bencher) {
-        b.iter(|| handle_fhash_ffloat_read_until_mut_par("../_data/measurements_1m.txt"));
+    fn bench_06_fhash_dfloat_read_until_mut(b: &mut Bencher) {
+        b.iter(|| handle_fhash_dfloat_read_until_mut("../_data/measurements_1m.txt"));
+    }
+
+    #[bench]
+    fn bench_07_fhash_dint_read_until_mut(b: &mut Bencher) {
+        b.iter(|| handle_fhash_dint_read_until_mut("../_data/measurements_1m.txt"));
+    }
+
+    #[bench]
+    fn bench_08_fhash_dint_read_until_mut_memchr(b: &mut Bencher) {
+        b.iter(|| handle_fhash_dint_read_until_mut_memchr("../_data/measurements_1m.txt"));
+    }
+
+    #[bench]
+    fn bench_09_fhash_dint_read_until_mut_memchr_cparse(b: &mut Bencher) {
+        b.iter(|| handle_fhash_dint_read_until_mut_memchr_cparse("../_data/measurements_1m.txt"));
+    }
+
+    #[bench]
+    fn bench_10_fhash_dfloat_read_until_mut_memchr_cparse_par(b: &mut Bencher) {
+        b.iter(|| handle_fhash_dint_read_until_mut_cparse_par("../_data/measurements_1m.txt"));
     }
 }
